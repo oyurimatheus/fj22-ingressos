6a21169a1685ae21e5f2cfeac31ba9ba63ec1fa3
Yuri Matheus <yurimatheusb@hotmail.com>
2018-01-20 14:41:14 -0200

    criando metodos e testes

--- src/main/java/br/com/caelum/ingresso/model/Sessao.java
+++ src/main/java/br/com/caelum/ingresso/model/Sessao.java
@@ -17,39 +17,52 @@ import javax.persistence.OneToMany;
 @Entity
 public class Sessao {
 
-    @Id
-    @GeneratedValue
-    private Integer id;
-    private LocalTime horario;
+	@Id
+	@GeneratedValue
+	private Integer id;
+	private LocalTime horario;
 
-    @ManyToOne
-    private Sala sala;
-    @ManyToOne
-    private Filme filme;
+	@ManyToOne
+	private Sala sala;
+	@ManyToOne
+	private Filme filme;
 
-    private BigDecimal preco;
-    
-    @OneToMany(mappedBy = "sessao", fetch = FetchType.EAGER)
-    private Set<Ingresso> ingressos = new HashSet<>();
-    
-    /**
-     *@deprecated hibernate only
-     */
+	private BigDecimal preco;
 
-    public Sessao() {}
+	@OneToMany(mappedBy = "sessao", fetch = FetchType.EAGER)
+	private Set<Ingresso> ingressos = new HashSet<>();
 
-    public Sessao(LocalTime horario, Filme filme, Sala sala) {
-        this.horario = horario;
-        this.filme = filme;
-        this.sala = sala;
-        this.preco = sala.getPreco().add(filme.getPreco());
-    }
+	/**
+	 * @deprecated hibernate only
+	 */
 
-    public Map<String, List<Lugar>> getMapaDeLugares() {
-    	return sala.getMapaDeLugares();
-    }
-    
-    public BigDecimal getPreco() {
+	public Sessao() {
+	}
+
+	public Sessao(LocalTime horario, Filme filme, Sala sala) {
+		this.horario = horario;
+		this.filme = filme;
+		this.sala = sala;
+		this.preco = sala.getPreco().add(filme.getPreco());
+	}
+
+	public boolean isDisponivel(Lugar lugarSelecionado) {
+		return ingressos.stream().map(Ingresso::getLugar).noneMatch(lugar -> lugar.equals(lugarSelecionado));
+	}
+
+	public Set<Ingresso> getIngressos() {
+		return ingressos;
+	}
+
+	public void setIngressos(Set<Ingresso> ingressos) {
+		this.ingressos = ingressos;
+	}
+
+	public Map<String, List<Lugar>> getMapaDeLugares() {
+		return sala.getMapaDeLugares();
+	}
+
+	public BigDecimal getPreco() {
 		return preco;
 	}
 
@@ -58,34 +71,34 @@ public class Sessao {
 	}
 
 	public Integer getId() {
-        return id;
-    }
+		return id;
+	}
 
-    public void setId(Integer id) {
-        this.id = id;
-    }
+	public void setId(Integer id) {
+		this.id = id;
+	}
 
-    public LocalTime getHorario() {
-        return horario;
-    }
+	public LocalTime getHorario() {
+		return horario;
+	}
 
-    public void setHorario(LocalTime horario) {
-        this.horario = horario;
-    }
+	public void setHorario(LocalTime horario) {
+		this.horario = horario;
+	}
 
-    public Sala getSala() {
-        return sala;
-    }
+	public Sala getSala() {
+		return sala;
+	}
 
-    public void setSala(Sala sala) {
-        this.sala = sala;
-    }
+	public void setSala(Sala sala) {
+		this.sala = sala;
+	}
 
-    public Filme getFilme() {
-        return filme;
-    }
+	public Filme getFilme() {
+		return filme;
+	}
 
-    public void setFilme(Filme filme) {
-        this.filme = filme;
-    }
+	public void setFilme(Filme filme) {
+		this.filme = filme;
+	}
 }
--- src/test/java/br/com/caelum/ingresso/model/SessaoTest.java
+++ src/test/java/br/com/caelum/ingresso/model/SessaoTest.java
@@ -3,22 +3,49 @@ package br.com.caelum.ingresso.model;
 import java.math.BigDecimal;
 import java.time.Duration;
 import java.time.LocalTime;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.junit.Test;
 
 import org.junit.Assert;
+import org.junit.Before;
 
 public class SessaoTest {
 	
+	private Sala sala;
+	private Filme filme;
+	private BigDecimal somaDosPrecosDaSalaEFilme;
+	private Sessao sessao;
+
+	@Before
+	public void preparaTestes() {
+		this.sala = new Sala("Eldorado - IMax", new BigDecimal("22.5"));
+		this.filme = new Filme("Rougue One", Duration.ofMinutes(120), "Sci-Fi", new BigDecimal("12.0"));
+		this.somaDosPrecosDaSalaEFilme = sala.getPreco().add(filme.getPreco());
+		this.sessao = new Sessao(LocalTime.parse("10:00:00"), filme, sala);
+	}
+	
 	@Test
 	public void oPrecoDaSessaoDeveSerIgualASomaDoPrecoDaSalaMaisOPrecoDoFilme() {
-		Sala sala = new Sala("Eldorado - IMax", new BigDecimal("22.5"));
-		Filme filme = new Filme("Rougue One", Duration.ofMinutes(120), "Sci-Fi", new BigDecimal("12.0"));
-		
-		BigDecimal somaDosPrecosDaSalaEFilme = sala.getPreco().add(filme.getPreco());
-		
-		Sessao sessao = new Sessao(LocalTime.parse("10:00:00"), filme, sala);
-		
 		Assert.assertEquals(somaDosPrecosDaSalaEFilme, sessao.getPreco());
+	}
+	
+	@Test
+	public void garanteQueOLugarA1EstaOcupadoEOsLugaresA2EA3Disponiveis() {
+		Lugar a1 = new Lugar("A", 1);
+		Lugar a2 = new Lugar("A", 2);
+		Lugar a3 = new Lugar("A", 3);
+		
+		Ingresso ingresso = new Ingresso(sessao, TipoDeIngresso.INTEIRO, a1);
+		
+		Set<Ingresso> ingressos = Stream.of(ingresso).collect(Collectors.toSet());
+		
+		sessao.setIngressos(ingressos);
+		
+		Assert.assertFalse(sessao.isDisponivel(a1));
+		Assert.assertTrue(sessao.isDisponivel(a2));
+		Assert.assertTrue(sessao.isDisponivel(a3));
 	}
 }
