fe5587a6fb6fd732dd31e4a915c8b3ebbfe6f981
Yuri Matheus <yurimatheusb@hotmail.com>
2018-01-20 14:34:26 -0200

    modificando metodo

--- src/main/java/br/com/caelum/ingresso/controller/SessaoController.java
+++ src/main/java/br/com/caelum/ingresso/controller/SessaoController.java
@@ -20,6 +20,7 @@ import br.com.caelum.ingresso.dao.SessaoDao;
 import br.com.caelum.ingresso.model.ImagemCapa;
 import br.com.caelum.ingresso.model.Sala;
 import br.com.caelum.ingresso.model.Sessao;
+import br.com.caelum.ingresso.model.TipoDeIngresso;
 import br.com.caelum.ingresso.model.form.SessaoForm;
 import br.com.caelum.ingresso.rest.ImdbClient;
 import br.com.caelum.ingresso.validacao.GerenciadorDeSessao;
@@ -60,6 +61,7 @@ public class SessaoController {
     	
     	view.addObject("sessao", sessao);
     	view.addObject("imagemCapa", imagemCapa.orElse(new ImagemCapa()));
+    	view.addObject("tiposDeIngressos", TipoDeIngresso.values());
     	
     	return view;
     }
