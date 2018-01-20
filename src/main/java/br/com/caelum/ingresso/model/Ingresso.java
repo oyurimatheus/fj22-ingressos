9a0c7c9237810c450adfe555cdbff80037ce5348
Yuri Matheus <yurimatheusb@hotmail.com>
2018-01-20 14:26:54 -0200

    adicionando anotacoes e arrumando testes

--- src/main/java/br/com/caelum/ingresso/model/Ingresso.java
+++ src/main/java/br/com/caelum/ingresso/model/Ingresso.java
@@ -2,13 +2,31 @@ package br.com.caelum.ingresso.model;
 
 import java.math.BigDecimal;
 
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
 import br.com.caelum.ingresso.model.descontos.Desconto;
 
+@Entity
 public class Ingresso {
 
+	@Id
+	@GeneratedValue
+	private Integer id;
+	
+	@ManyToOne
 	private Sessao sessao;
+	
 	private BigDecimal preco;
+	
+	@ManyToOne
 	private Lugar lugar;
+	
+	@Enumerated(EnumType.STRING)
 	private TipoDeIngresso tipoDeIngresso;
 	
 	/**
--- src/test/java/br/com/caelum/ingresso/model/descontos/DescontoTest.java
+++ src/test/java/br/com/caelum/ingresso/model/descontos/DescontoTest.java
@@ -10,39 +10,43 @@ import org.junit.Test;
 
 import br.com.caelum.ingresso.model.Filme;
 import br.com.caelum.ingresso.model.Ingresso;
+import br.com.caelum.ingresso.model.Lugar;
 import br.com.caelum.ingresso.model.Sala;
 import br.com.caelum.ingresso.model.Sessao;
+import br.com.caelum.ingresso.model.TipoDeIngresso;
 
 public class DescontoTest {
 
 	private Sala sala;
 	private Filme filme;
 	private Sessao sessao;
+	private Lugar lugar;
 
 	@Before
 	public void preparaDescontos() {
 		this.sala = new Sala("Eldorado - IMAX", new BigDecimal("20.5"));
 		this.filme = new Filme("Rougue One", Duration.ofMinutes(120), "Sci-Fi", new BigDecimal("12"));
 		this.sessao = new Sessao(LocalTime.parse("10:00:00"), filme, sala);
+		this.lugar = new Lugar("A", 1);
 	}
 	
 	@Test
 	public void naoDeveConcederDescontoParaIngressoNormal() {
-		Ingresso ingresso = new Ingresso(sessao, new SemDesconto());
+		Ingresso ingresso = new Ingresso(sessao, TipoDeIngresso.INTEIRO, lugar);
 		BigDecimal precoEsperado = new BigDecimal("32.5");
 		Assert.assertEquals(precoEsperado, ingresso.getPreco());
 	}
 	
 	@Test
 	public void deveConcederDescontoDe30PorcentoParaIngressosDeClientesDeBancos() {
-		Ingresso ingresso = new Ingresso(sessao, new DescontoParaBancos());
+		Ingresso ingresso = new Ingresso(sessao, TipoDeIngresso.BANCO, lugar);
 		BigDecimal precoEsperado = new BigDecimal("22.75");
 		Assert.assertEquals(precoEsperado, ingresso.getPreco());
 	}
 	
 	@Test
 	public void deveConcederDescontoDe50PorcentoParaIngressosDeClientesDeEstudante() {
-		Ingresso ingresso = new Ingresso(sessao, new DescontoParaEstudantes());
+		Ingresso ingresso = new Ingresso(sessao, TipoDeIngresso.ESTUDANTE, lugar);
 		BigDecimal precoEsperado = new BigDecimal("16.25");
 		Assert.assertEquals(precoEsperado, ingresso.getPreco());
 	}
